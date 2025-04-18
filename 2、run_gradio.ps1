c814a4e3ccec0657da57d096610fe32bcb028b7f
sdbds <865105819@qq.com>
2025-04-18 22:01:56 +0800

    init

---
+++ 2、run_gradio.ps1
@@ -0,0 +1,50 @@
+#Run Gradio script by @bdsqlsz
+
+$share = $false              # Enable sharing (equivalent to --share)
+$server = "127.0.0.1"          # Server address
+$port = ""                # Port number (leave as $null if not specified)
+$inbrowser = $true          # Auto-open in browser
+
+# ============= DO NOT MODIFY CONTENTS BELOW | 请勿修改下方内容 =====================
+# Activate python venv
+Set-Location $PSScriptRoot
+if ($env:OS -ilike "*windows*") {
+    if (Test-Path "./venv/Scripts/activate") {
+        Write-Output "Windows venv"
+        ./venv/Scripts/activate
+    }
+    elseif (Test-Path "./.venv/Scripts/activate") {
+        Write-Output "Windows .venv"
+        ./.venv/Scripts/activate
+    }
+}
+elseif (Test-Path "./venv/bin/activate") {
+    Write-Output "Linux venv"
+    ./venv/bin/Activate.ps1
+}
+elseif (Test-Path "./.venv/bin/activate") {
+    Write-Output "Linux .venv"
+    ./.venv/bin/activate.ps1
+}
+
+$Env:HF_HOME = "huggingface"
+#$Env:HF_ENDPOINT = "https://hf-mirror.com"
+$Env:XFORMERS_FORCE_DISABLE_TRITON = "1"
+$ext_args = [System.Collections.ArrayList]::new()
+
+if ($share) {
+    [void]$ext_args.Add("--share")
+}
+if ($server) {
+    [void]$ext_args.Add("--server=$server")
+}
+if ($port) {
+    [void]$ext_args.Add("--port=$port")
+}
+if ($inbrowser) {
+    [void]$ext_args.Add("--inbrowser")
+}
+
+# run Gradio
+python "./demo_gradio.py" `
+    $ext_args
